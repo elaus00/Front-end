3bafb75ef854169e7bb7d42591f3c0d8ff24e407
pp.navillera <pp.navillera@gmail.com>
2024-05-18 17:52:40 +0900

    fix: seperate Search Function

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,6 +1,7 @@
 import { ChevronDown } from "./ChevronDown/ChevronDown.jsx";
 import Search from "./Search/Search.jsx";
 import styles from "./Pureplate/Pureplate.module.css";
+import Search2 from "./Search2.jsx";
 
 function SearchBar() {
   return (
@@ -9,9 +10,11 @@ function SearchBar() {
         <div className={styles.vegan}>Vegan </div>
         <ChevronDown className={styles.chevronDownInstance}></ChevronDown>
       </div>
-      <div className={styles.search}>
-        <div className={styles.search2}>Search </div>
-      </div>
+      {/* <div className={styles.search}> */}
+      {/* <div className={styles.search2}>Search </div> */}
+
+      <Search2 />
+      {/* </div> */}
       <div className={styles.button}>
         <div className={styles.rectangle2}></div>
         <Search className={styles.searchInstance}></Search>
