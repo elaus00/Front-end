b83c7c0f4c6447c0afcd1abb0f0df52d055e66bb
pp.navillera <pp.navillera@gmail.com>
2024-05-23 03:11:51 +0900

    Refactor: refactoring for the code consistency

--- src/Pureplate/Pureplate.jsx
+++ src/Pureplate/Pureplate.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import styles from "./Pureplate.module.css";
 import MapNaverCur from "../Map/Map.js";
-// import Header from "./Header/header.jsx";
-// import { MapPin } from "../MapPin/MapPin.jsx";
-// import { Plus } from "../Plus/Plus.jsx";
-// import { Minus } from "../Minus/Minus.jsx";
 import Attributes from "../Attributes.js";
 import SearchBar from "../SearchBar.js";
 import Profile from "../Profile.jsx";
@@ -12,14 +8,13 @@ import Header from "../Header.js";
 // import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-const Pureplate = ({ className, ...props }) => {
+function Pureplate() {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.homeLogedIn + " " + className}>
+      <div className={styles.homeLogedIn}>
         <div className={styles.mapContainer}>
           <MapNaverCur />
         </div>
-        {/* <Header1 /> */}
       </div>
       <header
         className={styles.header}
@@ -36,10 +31,8 @@ const Pureplate = ({ className, ...props }) => {
         <Attributes />
         <Profile />
       </header>
-
-      {/* <Header /> */}
     </div>
   );
-};
+}
 
 export default Pureplate;
