66adf3b49aeb4600edbdbd7e60cc3b2ad109e78f
pp.navillera <pp.navillera@gmail.com>
2024-05-31 21:59:49 +0900

    Fix: change assets path

--- src/Attributes.js
+++ src/Attributes.js
@@ -3,7 +3,6 @@ import styles from "./Pureplate/Pureplate.module.css";
 // import { AttributesButtonSelectedFalse } from "./AttributesButtonSelectedFalse/AttributesButtonSelectedFalse.jsx";
 import Button from "./Button.js";
 import { useAuth } from "./AuthContext.jsx";
-import { useEffect } from "react";
 
 function Attributes() {
   const { dietToggle, setDietToggle } = useAuth();
@@ -11,7 +10,6 @@ function Attributes() {
 
   return (
     <div className={styles.attributes}>
-      {/* <img className={styles.filterIcon} src="filter-icon0.svg" /> */}
       <Button attribute={dietType[0]} />
       <Button attribute={dietType[1]} />
       <Button attribute={dietType[2]} />
--- src/ChevronDown/ChevronDown.jsx
+++ src/ChevronDown/ChevronDown.jsx
@@ -1,10 +1,11 @@
 import styles from "./ChevronDown.module.css";
+import chevronDown from "../assets/Icons/chevron-down.svg";
 
 export const ChevronDown = ({ className, ...props }) => {
   return (
     <img
       className={styles.chevronDown + " " + className}
-      src="chevron-down.svg"
+      src={chevronDown}
       alt="down"
     />
   );
