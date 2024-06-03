cba805a15884e5233989097c006b33338d58841a
pp.navillera <pp.navillera@gmail.com>
2024-06-03 22:25:34 +0900

    Design: mobile sign window

--- src/Profile/Signin/SignInInputField.jsx
+++ src/Profile/Signin/SignInInputField.jsx
@@ -9,18 +9,18 @@ function SignInInputField({
 }) {
   return (
     <div className={styles.signInTextBox}>
-      <div className={styles.frame31}>
-        <input
-          className={`${styles.placeholder} ${
-            errorMessage ? styles.errorInput : ""
-          }`}
-          value={value}
-          onChange={onChange}
-          type={type}
-          placeholder={errorMessage ? `⚠ ${errorMessage}` : label} // placeholder도 prop으로부터 받은 label을 사용합니다.
-          // aria-describedby={error ? "error-msg" : undefined}
-        />
-      </div>
+      {/* <div className={styles.frame31}> */}
+      <input
+        className={`${styles.placeholder} ${
+          errorMessage ? styles.errorInput : ""
+        }`}
+        value={value}
+        onChange={onChange}
+        type={type}
+        placeholder={errorMessage ? `⚠ ${errorMessage}` : label} // placeholder도 prop으로부터 받은 label을 사용합니다.
+        // aria-describedby={error ? "error-msg" : undefined}
+      />
+      {/* </div> */}
       <div className={styles.nameLabel}>{label}</div>
     </div>
   );
